2465771b1dfb2d7b36e61cbbb80d57ec292d91d0
Andy Ferguson <andy@teknostatik.org>
2021-07-25 07:08:44 +0100

    Created i3 installation script that can be used anywhere

---
+++ install_i3.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Script to install i3 WM on any Debian based distro
+# Tested on Debian, Ubuntu and Raspberry Pi OS
+
+echo "----------------------------------------"
+echo "i3 Installation Script - v0.1, July 2021"
+echo "----------------------------------------"
+
+# Standard error mitigation
+
+set -euo pipefail
+
+# Update software
+
+sudo apt update
+sudo apt -y upgrade
+
+# Install the i3 window manager and some basic utilities
+
+sudo apt install -y i3 i3blocks feh arandr scrot xautolock barrier kitty
+sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt install -y ulauncher
+
+# Set up i3. Comment this out if you want to use your own config file or build your config from scratch.
+
+wget https://raw.githubusercontent.com/teknostatik/i3_config/main/config
+wget https://raw.githubusercontent.com/teknostatik/i3_config/main/lock.sh
+mkdir .config/i3
+mv config ~/.config/i3/
+sudo mv lock.sh /usr/local/bin/
+
+# Set up i3 wallpaper
+# These are downloaded from various places. Will try and find credits at some point.
+
+sudo mkdir /usr/share/wallpaper
+# Copy any existing wallpapers into this new directory (delate any you don't like later)
+sudo cp -R /usr/share/backgrounds/* /usr/share/wallpaper
+# But there are also some different backgrounds I liketo use
+cd /usr/share/wallpaper
+sudo wget https://www.dropbox.com/s/0yg8txbgw0ifqmg/9dy0gvxq7fl61.png
+sudo wget https://www.dropbox.com/s/cljxhezhxuu3nce/background.png
+sudo wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
+sudo wget https://www.dropbox.com/s/1i7g8u5h6whd5dv/3430638.png
+sudo wget https://www.dropbox.com/s/bkae9ethe3jqbod/jyrji9bnp3171.jpg
+sudo wget https://www.dropbox.com/s/idk05cia43lj5qb/rocket.png?dl=0
+cd $HOME
+wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
+sudo mv randomise_wallpaper /usr/local/bin/
+sudo chmod 755 /usr/local/bin/randomise_wallpaper
+
+echo "------------------------------------------------------------------------"
+echo "i3 is now installed. Log out and choose it from your window manager menu"
+echo "------------------------------------------------------------------------"
