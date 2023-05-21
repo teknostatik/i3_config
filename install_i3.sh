612ed8df7481141c850b07501ef375751f66d18e
Andy Ferguson <andy@teknostatik.org>
2023-05-21 20:32:03 +0100

    Simplifying wallpaper

--- install_i3.sh
+++ install_i3.sh
@@ -5,9 +5,9 @@
 # Only installs what's needed for i3, so should be called from other scripts
 # It assumes you are logged in as the user who will be using i3
 
-echo "-------------------------------------------"
-echo "i3 Installation Script - v1.0, October 2021"
-echo "-------------------------------------------"
+echo "----------------------"
+echo "i3 Installation Script"
+echo "----------------------"
 
 # Install the i3 window manager and some basic utilities (all of these are referenced in my i3 config file, so need to be installed)
 
@@ -28,20 +28,11 @@ mkdir ~/.config/polybar
 mv polybar_config ~/.config/polybar/config
 
 # Set up i3 wallpaper
-# These are downloaded from various places. Will try and find credits at some point.
 
 sudo mkdir /usr/share/wallpaper
 # Copy any existing wallpapers into this new directory (delete any you don't like later)
 sudo cp -R /usr/share/backgrounds/* /usr/share/wallpaper
-# But there are also some different backgrounds I like to use
-cd /usr/share/wallpaper
-sudo wget https://www.dropbox.com/s/65qlzytfq8c2thu/1920x1080.jpg
-sudo wget https://www.dropbox.com/s/mxlrmmlmz7cvcan/undefined%20-%20Imgur.png
-sudo wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
-sudo wget https://www.dropbox.com/s/5g16o13gauzfabg/undefined%20-%20Imgur%281%29.jpg
-sudo wget https://www.dropbox.com/s/wr7zeamyfickq6z/undefined%20-%20Imgur%281%29.png
-sudo wget https://www.dropbox.com/s/idk05cia43lj5qb/rocket.png
-sudo wget https://www.dropbox.com/s/vev7hiio2zwff2w/undefined%20-%20Imgur%284%29.jpg
+
 # In my i3 config file we switch wallpaper using MOD + Z, but this requires a script
 cd $HOME
 wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
