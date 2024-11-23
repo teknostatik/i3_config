cbe733a5b3f5a95084e4082fcd8474d105a4fb3b
Andy Ferguson <andy@teknostatik.org>
2024-11-23 11:26:52 +0000

    Added lxappearance and reformatted

--- install_i3.sh
+++ install_i3.sh
@@ -11,7 +11,20 @@ echo "----------------------"
 
 # Install the i3 window manager and some basic utilities (all of these are referenced in my i3 config file, so need to be installed)
 
-sudo apt install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine picom
+sudo apt install -y \
+    i3 \
+    i3blocks \
+    feh \
+    arandr \
+    scrot \
+    xautolock \
+    barrier \ 
+    kitty \
+    imagemagick \
+    polybar \
+    caffeine \
+    picom \ 
+    lxappearance
 
 # Set up i3. Comment this out if you want to use your own config file or build your config from scratch.
 
